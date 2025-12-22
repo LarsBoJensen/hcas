fd60b9441f0677abe216d666c5f591244c4c805d
Lars Bo Jensen <lb@larsbojensen.eu>
2025-12-22 17:12:54 +0100

    Created HCAS subtheme, added a new paragraph type (external link) and a twig template for it

---
+++ web/themes/hcas/js/hcas.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * hcas behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.hcas = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));

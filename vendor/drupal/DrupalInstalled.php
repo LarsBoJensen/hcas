f75da2f5acdf4098dcb26f677981cf42e345b96f
Lars Bo Jensen <lb@larsbojensen.eu>
2025-12-28 08:32:48 +0100

    feat: installs and configures the Redirect module

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = 'af3816a3c4104320';
+  public const string VERSIONS_HASH = '3735581b7e847b8b';
 
 }
